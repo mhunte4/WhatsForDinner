879f8a9dfd40197f80b55997cf62d7a7cc456268
Mark Hunter <mhunte4@gmail.com>
2024-05-13 10:31:08 -0500

    Initial Commit

---
+++ WhatsForDinner/WhatsForDinnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsForDinnerApp.swift
+//  WhatsForDinner
+//
+//  Created by Mark Hunter on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct WhatsForDinnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
